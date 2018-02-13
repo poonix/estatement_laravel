1d1a88abf3926fce1c32f2fe378d4744dc72596f
poonix <poonix27@gmail.com>
2018-02-13 18:50:29 +0700

    updated

--- app/Http/NameAPI.php
+++ app/Http/NameAPI.php
@@ -5,5 +5,5 @@ namespace App\Http;
 class NameAPI
 {
 	public static $GetListCollection ="/DATA/GetList";
-
+	public static $GetListDataPembiayaan ="/get_rekening_debitur/";
 }
--- app/Http/ParameterAPI.php
+++ app/Http/ParameterAPI.php
@@ -5,5 +5,6 @@ namespace App\Http;
 class ParameterAPI
 {
 	public static $APICollection ="http://182.23.52.252:3333";
+	public static $APIPembiayaan ="http://182.23.52.249/PNM/api/v1/mms";
 
 }
