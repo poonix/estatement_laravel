d3a2e3970df96c95bee9986bde567716bbc79f87
poonix <poonix27@gmail.com>
2018-02-13 21:39:20 -0800

    header

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -40,7 +40,7 @@
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 <!-- The user image in the navbar-->
                 
-                   <img src="@if(Session::get('foto')) {{ asset('themes/admin/dist/img/'.Session::get('foto'))}}  @else {{ asset('themes/admin/dist/img/user2-160x160.jpg')}} @endif" class="user-image" alt="Image">
+                   <img src="{{ asset('themes/admin/dist/img/user2-160x160.jpg')}}" class="user-image" alt="Image">
                 
                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
                 <span class="hidden-xs">{{ Session::get('nama') }}</span>
@@ -48,37 +48,15 @@
               <ul class="dropdown-menu">
                 <!-- The user image in the menu -->
                 <li class="user-header">
-                  @if(Session::get('nama') == null || Session::get('foto') == '' )  
+                 
                 <img src="{{ asset('themes/admin/dist/img/user2-160x160.jpg')}}" class="user-image" alt="{{ Session::get('nama') }} Image">
-                @else 
-                <img src="{{ asset('themes/admin/dist/img/'.Session::get('foto'))}}" class="user-image" alt="{{ Session::get('nama') }} Image">
-                @endif
 
                   <p>
                     {{ Session::get('nama') }}
-                    <small>Member since Nov. 2012</small>
                   </p>
-                </li>
-                <!-- Menu Body -->
-                <li class="user-body">
-                  <div class="row">
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Followers</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Sales</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Friends</a>
-                    </div>
-                  </div>
-                  <!-- /.row -->
                 </li>
                 <!-- Menu Footer-->
                 <li class="user-footer">
-                  <div class="pull-left">
-                    <a href="#" class="btn btn-default btn-flat">Profile</a>
-                  </div>
                   <div class="pull-right">
                     <a href="{{ url('/logout') }}" class="btn btn-default btn-flat">Sign out</a>
                   </div>
